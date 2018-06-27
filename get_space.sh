1c3a4ae8faed077571d84ebc4873f6ee76d6f5df
Matthew Ho <maho3@illinois.edu>
2018-06-27 17:32:36 -0400

    First commit. Most of the work is already done, but this repo is just for my own future reference.

---
+++ get_space.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Get new objects at http://skyserver.sdss.org/dr7/en/tools/search/form/default.aspx
+# See user guide at http://skyserver.sdss.org/dr7/en/tools/search/form/guide.asp#mags
+
+# Convert 2 images to 1 using ImageMagick's command $convert.
+# This is useful if your screen resolution is higher than SkyServer's resolution limit 1920x1920
+
+cd /home/annam/Desktop/git/space_wall
+
+len=$(($(cat objects.csv | wc -l) - 1))
+
+end=$((SECONDS+60*60*8))
+
+while [ $SECONDS -lt $end ]; do
+    i=$((2+RANDOM%$len))
+    
+    printf "\nrow: $i\n"
+
+    ra=$(sed -n "$i p" objects.csv | cut -d, -f 2)
+    dec=$(sed -n "$i p" objects.csv | cut -d, -f 3)
+
+    wget -nv -O "/home/annam/Pictures/Wallpapers/sdss.jpg" "http://skyserver.sdss.org/dr12/SkyserverWS/ImgCutout/getjpeg?ra=$ra&dec=$dec&width=1920&height=1080"
+
+    sleep 30s
+    
+done
